9aae7b6534a103bee479c27f965fdf821b41362b
sbartulovic-ossunist <92647475+StipeBartulovic@users.noreply.github.com>
2021-11-15 17:55:47 +0100

    Add files via upload

---
+++ Vjezba3_oop/Vj3Zadatak4/header.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+void pushInElements(std::vector<int>& vect, int n);
+
+void removeElement(std::vector<int>& vect, int n);
+
+void printVector(std::vector<int> vect);
