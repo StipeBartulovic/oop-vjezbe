974e87c1f77e815b7f497602b54c80b78ac35ef7
sbartulovic-ossunist <92647475+StipeBartulovic@users.noreply.github.com>
2021-11-15 20:43:52 +0100

    Add files via upload

---
+++ Vjezba3_oop/Vj3Zadatak2/func.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <vector>
+
+void vectorAndNumOfElements(std::vector<int>& vect, int n);
+
+void vectorAndInterval(std::vector<int>& vect, int a, int b);
+
+std::vector<int> isInFirstNotInSecond(std::vector<int> first, std::vector<int> second);
+
+void printVector(std::vector<int> vect, int n);
