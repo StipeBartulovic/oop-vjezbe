84203d72f5e3a7f8231b2549415cb06bde7ad6dd
sbartulovic-ossunist <92647475+StipeBartulovic@users.noreply.github.com>
2021-11-15 17:54:56 +0100

    Update main.cpp

--- Vjezba3_oop/Vj3Zadatak1/main.cpp
+++ Vjezba3_oop/Vj3Zadatak1/main.cpp
@@ -3,25 +3,36 @@
 #include "myVector.hpp"
 using namespace std;
 
-void vectorInput(vector<int>& vect, int n) {
-    int num = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-        vect.push_back(num);
+int main()
+{
+    int n1, n2, a, b;
+    vector <int> vect1, vect2;
+
+    cout << "Enter the size of array:" << endl;
+    cin >> n1;
+
+    cout << "Enter the elements of array :" << endl;
+    vectorInput(vect1, n1);
+
+    cout << "Enter the range of numbers for the new array:" << endl;
+    cin >> a;
+    cin >> b;
+
+    if (a > b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
     }
+
+    cout << "Enter array size:" << endl;
+    vectorAndInterval(vect2, a, b);
+    
+    n2 = vect2.size();
+
+    cout << "First array:";
+    printVector(vect1, n1);
+    cout << endl << "Second array: ";
+    printVector(vect2, n2);
+
 }
 
-void vectorAndInterval(vector<int>& vect, int a, int b) {
-    int num = 0;
-    cin >> num;
-    while ((num > a) && (num < b)) {
-        vect.push_back(num);
-        cin >> num;
-    }
-}
-
-void printVector(vector<int> vect, int n) {
-    for (int i = 0; i < n; i++) {
-        cout << vect[i] << " ";
-    }
-}
